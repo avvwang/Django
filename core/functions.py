edbf57cb018a34a5dfe3831767de6c87e55215c6
heliang <avvwang@qq.com>
2019-04-28 20:38:15 +0800

    更新文件

--- core/functions.py
+++ core/functions.py
@@ -84,6 +84,7 @@ def uploadfile(ftp, remotepath, localpath):
     ftp.set_debuglevel(0)
     ftp.close()
 def test(data):
+    print(data)
     try:
         name = data.get("key")
         id = data.get("key1")
